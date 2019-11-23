f32aca84a3400ac2e204bf64618580e55b831d8b
kanemil <kanemil1@yahoo.com>
2019-11-23 09:49:44 +0300

    + fab disappear on decks fragment

---
+++ app/src/main/java/com/kanemil/memorich/presentation/view/fragments/utils/ScrollAwareFabBehavior.java
@@ -0,0 +1,56 @@
+package com.kanemil.memorich.presentation.view.fragments.utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ScrollAwareFabBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollAwareFabBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                                       @NonNull FloatingActionButton child,
+                                       @NonNull View directTargetChild,
+                                       @NonNull View target,
+                                       int axes,
+                                       int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(
+                        coordinatorLayout, child, directTargetChild, target, axes, type);
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                               @NonNull FloatingActionButton child,
+                               @NonNull View target,
+                               int dxConsumed,
+                               int dyConsumed,
+                               int dxUnconsumed,
+                               int dyUnconsumed,
+                               int type) {
+        super.onNestedScroll(
+                coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+        if (dyConsumed > 0 && child.isShown()) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onHidden(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (dyConsumed < 0 && !child.isShown()) {
+            child.show();
+        }
+    }
+}
