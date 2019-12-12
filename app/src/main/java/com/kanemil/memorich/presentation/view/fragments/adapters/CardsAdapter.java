88bd4fc587b3306fd48fa29026b30c248d07262a
kanemil <kanemil1@yahoo.com>
2019-12-12 12:50:22 +0300

    card's view edited

--- app/src/main/java/com/kanemil/memorich/presentation/view/fragments/adapters/CardsAdapter.java
+++ app/src/main/java/com/kanemil/memorich/presentation/view/fragments/adapters/CardsAdapter.java
@@ -66,7 +66,7 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardHolder>
                 break;
             case TRAINING:
             default:
-                cardLayout = R.layout.item_card_page_experimental;
+                cardLayout = R.layout.item_card_page;
                 break;
         }
         View root = LayoutInflater.from(parent.getContext())
